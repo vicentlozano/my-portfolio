b5d5f3dda08e8130ef7f40b98c8f93b7ca6c7a20
vicentlozano <vicentlozano@users.noreply.github.com>
2025-07-07 16:05:04 +0000

    deploy: 10d81a3473caaf49c52665c43bc9a9909788b171

--- assets/use-quasar-BhcWrEQ8.js
+++
@@ -1,1 +0,0 @@
-import{ad as a,aq as r}from"./index-uoorbQ6n.js";function u(){return a(r)}export{u};
---
+++ assets/use-quasar-D_Darast.js
@@ -0,0 +1,1 @@
+import{ad as a,aq as r}from"./index-DoluP-d6.js";function u(){return a(r)}export{u};
