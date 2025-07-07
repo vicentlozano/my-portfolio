e6e8a70f574b6e1bcb35a61b1778f57c247172aa
vicentlozano <vicentlozano@users.noreply.github.com>
2025-07-07 15:36:46 +0000

    deploy: f136eb4c932e36dd1f944e0e132f8b3a87808fb2

---
+++ assets/use-quasar-CsuGsxmx.js
@@ -0,0 +1,1 @@
+import{ad as a,aq as r}from"./index-rCoX_Fw5.js";function u(){return a(r)}export{u};
--- assets/use-quasar-Czmy3vH6.js
+++
@@ -1,1 +0,0 @@
-import{ad as a,aq as r}from"./index-C4sAGwLs.js";function u(){return a(r)}export{u};
